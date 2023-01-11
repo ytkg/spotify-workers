95ccfad9effe41cf37b9b93cb59c7ca464f7dfaf
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-01-11 23:33:30 +0900

    Enable Cache

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
 import { getAccessToken, getPlayingInfo } from './lib/spotify'
 
 const app = new Hono()
+
+app.use('/playing', cache({ cacheName: 'playing', cacheControl: 'max-age=10'}))
 
 app.get('/playing', async (c) => {
   const accessToken = await getAccessToken(c.env.SPOTIFY_CLIENT_ID, c.env.SPOTIFY_CLIENT_SECRET, c.env.SPOTIFY_REFRESH_TOKEN)
